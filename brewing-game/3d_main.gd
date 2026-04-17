3f7bf37d5618c3eac0e9c433328c2b9410399ea7
BIIGORANGE <bigorangecreations@gmail.com>
2026-04-16 20:25:53 -0700

    All labels and buttons finished
    - finished labels (not visually)
    - created functionality for permanent upgrades (fruits)
    
    TODO:
    - beautify buttons
    - procedurally generate floors textures

--- brewing-game/3d_main.gd
+++ brewing-game/3d_main.gd
@@ -9,7 +9,7 @@ func add_new_floor():
 	#floors_scene var needed here to instance new floor each time func add_new_floor() is called
 	var floors_scene = floors.instantiate()
 	add_child(floors_scene)
-	floors_scene.set_global_position(Vector3(0,0.5 + height_tracker,0))
+	floors_scene.set_global_position(Vector3(0,1.5 + height_tracker,0))
 	height_tracker += 1
 
 func _ready() -> void:
--- brewing-game/ControlMain.gd
+++ brewing-game/ControlMain.gd
@@ -13,6 +13,10 @@ extends Node
 @onready var total_coffee_label: Label = $TotalCoffeeCount
 @onready var floor_cost_label: Label = $FloorCost
 @onready var auto_brewers_label: Label = $AutoBrewers
+@onready var multiplier_label: Label = $Multiplier
+@onready var fruits_label: Label = $Fruits
+@onready var floors_counter: Label = $FloorsCounter
+
 @onready var auto_brew_timer: Timer = $AutoBrewTimer
 
 #reference to Node3D where all physical world items exist. added this to be able to access 
@@ -22,13 +26,16 @@ extends Node
 #all other variables
 var fruits = 0
 var floors = 1
-var floor_cost = 0
+var floor_cost = 50
 var coffee_total_counter = 0.0
 var coffee_counter = 0.0
-var multiplier = 1.0
+var brew_multiplier = 1.0
+var brew_multiplier_perm = 0
 var upgrade_cost = 10.0
-var fruit_upgrade_cost = 0
+var fruit_upgrade_cost = 1
 var auto_brewers = 0
+var auto_brewer_perm = 0
+var ui_visiblity = true
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	auto_brew_timer.start()
@@ -36,16 +43,19 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	#will add toggleable ui visibilty if time allows later
+	#if Input.is_action_just_pressed("toggle_ui_visibility"):
+		#toggle_ui_visibility()
+		pass
 
 func _on_brew_pressed() -> void:
-	coffee_total_counter = snapped(coffee_total_counter + ((1 * multiplier)* floors), 0.1)
-	coffee_counter = snapped(coffee_counter + ((1 * multiplier) * floors), 0.1)
+	coffee_total_counter = snapped(coffee_total_counter + ((1 * (brew_multiplier + brew_multiplier_perm))* floors), 0.1)
+	coffee_counter = snapped(coffee_counter + ((1 * (brew_multiplier + brew_multiplier_perm)) * floors), 0.1)
 	update_labels()
 
 func _on_upgrade_pressed() -> void:
 	if coffee_counter >= upgrade_cost:
-		multiplier += 0.1
+		brew_multiplier += 0.1
 		coffee_counter = snapped(coffee_counter - upgrade_cost, 0.1)
 		upgrade_cost = snapped(upgrade_cost * 1.2, 0.1)
 		update_labels()
@@ -63,8 +73,8 @@ func _on_auto_brew_upgrade_pressed() -> void:
 
 
 func _on_auto_brew_timer_timeout() -> void:
-	coffee_counter = snapped(coffee_counter + (auto_brewers * floors), 0.1)
-	coffee_total_counter = snapped(coffee_total_counter + (auto_brewers * floors), 0.1)
+	coffee_counter = snapped(coffee_counter + (auto_brewers + auto_brewer_perm * floors), 0.1)
+	coffee_total_counter = snapped(coffee_total_counter + (auto_brewers + auto_brewer_perm * floors), 0.1)
 	update_labels()
 
 func _on_new_floor_pressed() -> void:
@@ -76,7 +86,9 @@ func _on_new_floor_pressed() -> void:
 		upgrade_cost = 10
 		upgrade_cost = upgrade_cost * floors
 		auto_brewers = 0
-		multiplier = 1
+		auto_brewers += auto_brewer_perm
+		brew_multiplier = 1
+		brew_multiplier += brew_multiplier_perm
 		update_labels()
 		_3d_main.add_new_floor()
 		fruits += 1
@@ -84,19 +96,36 @@ func _on_new_floor_pressed() -> void:
 		print("need more coffee!")
 
 func _on_banana_upgrade_pressed() -> void:
-	#do some permanent upgrade
-	fruit_upgrade_cost +=1
-	update_labels()
+	if fruits >= fruit_upgrade_cost:
+		fruits -= fruit_upgrade_cost
+		fruit_upgrade_cost +=1
+		brew_multiplier_perm += 0.1
+		update_labels()
 
 func _on_strawberry_upgrade_pressed() -> void:
-	#do some other permanent upgrade.
-	fruit_upgrade_cost +=1
-	update_labels()
+	if fruits >= fruit_upgrade_cost:
+		fruits -= fruit_upgrade_cost
+		fruit_upgrade_cost +=1
+		auto_brewer_perm += 1
+		update_labels()
 
 func update_labels():
-	total_coffee_label.text = str(coffee_total_counter)
-	coffee_counter_label.text = str(coffee_counter)
+	total_coffee_label.text = "Total Coffee brewed: " + str(coffee_total_counter)
+	coffee_counter_label.text = "Coffee: " + str(coffee_counter)
 	upgrade_cost_label.text = "Upgrade Cost: "+ str(upgrade_cost)
-	auto_brewers_label.text = "Auto Brewers: " + str(auto_brewers)
+	auto_brewers_label.text = "Auto Brewers: " + str(auto_brewers + auto_brewer_perm)
 	floor_cost_label.text = "Floor Cost: " + str(floor_cost)
 	fruit_upgrade_cost_label.text = "Fruits Needed: " + str (fruit_upgrade_cost)
+	multiplier_label.text = "Multiplier: " + str(brew_multiplier + brew_multiplier_perm)
+	fruits_label.text = "Fruits: " + str(fruits)
+	floors_counter.text = "Floors: " + str(floors)
+	
+func toggle_ui_visibility():
+	if ui_visiblity:
+		ui_visiblity = false
+		brew.visible = false
+		print("working")
+	if !ui_visiblity:
+		ui_visiblity = true
+		brew.visible = true
+		print("working_!")
